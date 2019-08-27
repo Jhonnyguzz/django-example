d91f4a3a7d754d52163a4414746d9181b76b1c4c
Jhonatan Guzman <Jhonatan.Guzman@endava.com>
2019-08-27 12:43:44 -0500

    Fixing files

--- gallery/views.py
+++ gallery/views.py
@@ -2,12 +2,16 @@ from django.urls import reverse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Image, ImageForm
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
     images_list = Image.objects.all()
     context = {'images_list':images_list}
     return render(request, 'gallery/index.html', context)
+    #Serialize as json
+    #return HttpResponse(serializers.serialize("json", images_list))
 
 
 def add_image(request):
--- inception/settings.py
+++ inception/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'gallery',
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'inception.urls'
